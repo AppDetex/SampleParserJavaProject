72817d3b07ad5bdebf7d3d9d19978d3c2a69476d
novelrefrain <zclegg@gmail.com>
2016-03-21 22:00:42 -0700

    Update Main.java
    My implementation of the Sample Parser Java Project.

--- src/main/java/com/appdetex/sampleparserjavaproject/Main.java
+++ src/main/java/com/appdetex/sampleparserjavaproject/Main.java
@@ -1,16 +1,42 @@
 package com.appdetex.sampleparserjavaproject;
+import java.io.IOException;
 
+import org.jsoup.*;
+import org.jsoup.nodes.*;
+import org.jsoup.select.*;
+import org.jsoup.select.Selector.*;
 /**
- * Main Java Class
- *
- * This class will use Jsoup to retrieve a provided URL
- * and parse out certain data, printing that data to
- * stdout in a JSON format.
+ * Takes a URL for a Google Play app page as a command line parameter. Uses Jsoup to retrieve the URL,
+ * and parse it. Then outputs a JSON-formatted string containing
+ *      app title,
+ *      description,
+ *      publisher name,
+ *      price,
+ *      and rating (average review score)
+ * for the app to stdout.
+ * 
+ * @author Zach Clegg
  */
 public class Main {
 
-    public static void main( String[] args ) {
-        // Put code here
-    }
+	public static void main(String[] args) throws IOException, SelectorParseException {
+		StringBuilder appData = new StringBuilder("");
 
+		if (args.length > 0) {		//Check that we have been given input
+	        Document doc = Jsoup.connect(args[0]).get();	//Create a connection to the URL, then get the DOM data and return it as a Document
+	        Elements appTitle = doc.select(".id-app-title");	//Select the elements from the Document with the specified classes and attributes
+	        Elements description = doc.select(".show-more-content.text-body");
+	        Elements publisher = doc.select(".document-subtitle.primary span");
+	        Elements price = doc.select(".price.buy.id-track-click [itemprop=price]");
+	        Elements rating = doc.select(".score");
+	        appData.append("{\n\t\"title\": ").append(appTitle.text());		//Build our JSON-formatted string of outputs
+        	appData.append(",\n\t\"description\": ").append(description.text())
+        	.append(",\n\t\"publisher\": ").append(publisher.text())
+        	.append(",\n\t\"price\": ").append(price.attr("content"))
+        	.append(",\n\t\"rating\": ").append(rating.text() + "\n}");
+		}
+
+		System.out.println(appData.toString());		//Output the requested app data
+		return;
+	}
 }
