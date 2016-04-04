353aa59368b529117bce3856f3ee62c3c174c6b7
HarishArelli <arelly.harish08@gmail.com>
2016-04-04 00:29:45 -0600

    Update Main.java

--- src/main/java/com/appdetex/sampleparserjavaproject/Main.java
+++ src/main/java/com/appdetex/sampleparserjavaproject/Main.java
@@ -1,16 +1,53 @@
-package com.appdetex.sampleparserjavaproject;
+import java.io.InputStream;
+import java.net.URL;
 
-/**
- * Main Java Class
- *
- * This class will use Jsoup to retrieve a provided URL
- * and parse out certain data, printing that data to
- * stdout in a JSON format.
- */
-public class Main {
 
-    public static void main( String[] args ) {
-        // Put code here
-    }
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 
+
+public class Main 
+{
+	
+	/* Through 42matters.com Android API, I can connect to any app and get details of that app in the form of JSON Data. 
+	 * By replacing com.xample.airnavigation with com.facebook.katana in the URL we will get the title, description, publisher, price, rating of Facebook app.  
+	 */
+	public static void main(String args[]) throws Exception 
+	{
+	 URL url = new URL("https://42matters.com/api/1/apps/lookup.json?p=com.xample.airnavigation&access_token=722e1644744dabedf118fdde9a3f185c4d4f8b28");
+	 try (InputStream is = url.openStream();
+	       JsonReader rdr = Json.createReader(is)) 
+	       {
+		 	JsonObject obj = rdr.readObject();
+		 
+		 	System.out.println("{");
+		 	System.out.print("	\"title\":");
+		 	System.out.print(" \"");
+		 	System.out.print(obj.getString("title"));
+		 	System.out.println("\",");
+		 	
+		 	System.out.print("	\"description\":");
+		 	System.out.print(" \"");
+		 	System.out.print(obj.get("description"));
+		 	System.out.println("\",");
+		 	
+		 	System.out.print("	\"publisher\":");
+		 	System.out.print(" \"");
+		 	System.out.print(obj.get("developer"));
+		 	System.out.println("\",");
+
+		 	System.out.print("	\"price\":");
+		 	System.out.print(" \"");
+		 	System.out.print(obj.get("price"));
+		 	System.out.println("\",");
+		 	
+		 	System.out.print("	\"rating\": ");
+		 	System.out.println(obj.get("rating"));
+		 	System.out.print("}");
+		 	
+		 	
+	
+	 }
+}
 }
