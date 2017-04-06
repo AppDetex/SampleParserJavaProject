7280adc064a1caf172cd9268d54ba377f8d00e9e
Justin Podzimek <justin@macbook.local>
2017-04-06 16:57:54 -0600

    Return JSON results for provided URL

--- src/main/java/com/appdetex/sampleparserjavaproject/Main.java
+++ src/main/java/com/appdetex/sampleparserjavaproject/Main.java
@@ -1,5 +1,11 @@
 package com.appdetex.sampleparserjavaproject;
 
+import com.google.gson.Gson;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
 /**
  * Main Java Class
  *
@@ -10,7 +16,23 @@ package com.appdetex.sampleparserjavaproject;
 public class Main {
 
     public static void main( String[] args ) {
-        // Put code here
+
+        try {
+
+            Document doc = Jsoup.connect(args[0]).get();
+
+            ResultsModel model = new ResultsModel();
+            model.setTitle(doc.select(".id-app-title").text());
+            model.setDescription(doc.select("[itemprop=\"description\"] > div").html().split("(<br */?>)")[0]);
+            model.setPublisher(doc.select("[itemprop=\"author\"] [itemprop=\"name\"]").text());
+            model.setPrice(doc.select("[itemprop=\"price\"]").attr("content"));
+            model.setRating(Double.valueOf(doc.select(".score-container .score").text()));
+
+            System.out.println(new Gson().toJson(model));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
---
+++ src/main/java/com/appdetex/sampleparserjavaproject/ResultsModel.java
@@ -0,0 +1,53 @@
+package com.appdetex.sampleparserjavaproject;
+
+/**
+ * Created by justin on 4/6/17.
+ */
+public class ResultsModel {
+
+    private String title;
+    private String description;
+    private String publisher;
+    private String price;
+    private Double rating;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+}
