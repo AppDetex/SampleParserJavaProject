43019260cc3385b1f0ad2589fd91bb396f1cb88f
Ryan Dammrose <ryandammrose@gmail.com>
2017-06-03 21:50:18 -0600

    Functional jsoup read

--- src/main/java/com/appdetex/sampleparserjavaproject/Main.java
+++ src/main/java/com/appdetex/sampleparserjavaproject/Main.java
@@ -1,9 +1,13 @@
 package com.appdetex.sampleparserjavaproject;
 
+import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
-import org.apache.commons.cli.CommandLine;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
 
 /**
  * Main Java Class
@@ -14,7 +18,7 @@ import org.apache.commons.cli.CommandLine;
  */
 public class Main {
 
-    public static void main( String[] args ) {
+    public static void main( String[] args ) throws IOException {
         CommandLine line = null;
         Options options = new Options();
 
@@ -29,6 +33,13 @@ public class Main {
 
         if (line.hasOption("url")) {
             System.out.print(line.getOptionValue("url"));
+            String requestedUrl = line.getOptionValue("url");
+            String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36";
+            Document doc = Jsoup.connect(requestedUrl).userAgent(userAgent).get();
+            System.out.print(doc.toString());
+            // TODO: Make HTTP Call
+            // TODO: Parse HTTP Call
+            // TODO: Return JSON
         }
     }
 
