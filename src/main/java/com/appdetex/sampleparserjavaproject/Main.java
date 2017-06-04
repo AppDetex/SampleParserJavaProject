3b1ba40989ba000e4912d6c076e3ffc4da08a38a
Ryan Dammrose <ryandammrose@gmail.com>
2017-06-03 21:29:08 -0600

    basic functional cli

--- src/main/java/com/appdetex/sampleparserjavaproject/Main.java
+++ src/main/java/com/appdetex/sampleparserjavaproject/Main.java
@@ -1,5 +1,10 @@
 package com.appdetex.sampleparserjavaproject;
 
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.CommandLine;
+
 /**
  * Main Java Class
  *
@@ -10,7 +15,21 @@ package com.appdetex.sampleparserjavaproject;
 public class Main {
 
     public static void main( String[] args ) {
-        // Put code here
+        CommandLine line = null;
+        Options options = new Options();
+
+        options.addOption("url", true, "The Google Play Application URL you wish to Query.");
+
+        CommandLineParser parser = new DefaultParser();
+        try {
+            line = parser.parse(options, args);
+        } catch (Exception e) {
+            System.err.println("Parsing failed.  Reason: " + e.getMessage());
+        }
+
+        if (line.hasOption("url")) {
+            System.out.print(line.getOptionValue("url"));
+        }
     }
 
 }
