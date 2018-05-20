67a738fd0fec48be6ed2cac2e8498a1c5dd30502
Jacob Fjermestad <jacob.fjermestad@tangocard.com>
2018-05-20 11:41:42 -0600

    ask me about how weird this was to debug

--- src/main/java/com/appdetex/sampleparserjavaproject/Main.java
+++ src/main/java/com/appdetex/sampleparserjavaproject/Main.java
@@ -9,12 +9,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
 @SpringBootApplication
-@EnableAutoConfiguration
 @RequiredArgsConstructor
 public class Main implements CommandLineRunner {
 
