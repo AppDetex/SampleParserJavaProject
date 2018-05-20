20fcc015895aa49225734c05c1fa96d656e10440
Jacob Fjermestad <jacob.fjermestad@tangocard.com>
2018-05-19 18:08:20 -0600

    did not know this was possible

---
+++ README.md
@@ -0,0 +1,11 @@
+# SampleParserJavaProject
+Will hopefully parse Google Play websites for different chunks of data given a url via terminal prompt!
+
+### Build me up Buttercup
+You should use the all powerful and mighty Maven!
+```bash
+mvn clean package
+```
+
+### Running with the Devil
+TBD
--- src/main/java/com/appdetex/sampleparserjavaproject/Main.java
+++ src/main/java/com/appdetex/sampleparserjavaproject/Main.java
@@ -1,7 +1,19 @@
 package com.appdetex.sampleparserjavaproject;
 
-public class Main {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class Main implements CommandLineRunner {
     public static void main( String[] args ) {
-        // Put code here
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("WUT WUT SPRING BOOT CAN DO CLI APPS?!");
     }
 }
