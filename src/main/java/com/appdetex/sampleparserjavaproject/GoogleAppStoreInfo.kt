08286bf5eccddb53761b9e1d64ea225a04afa3e6
Robert R. Meyer <blue.dog.archolite@gmail.com>
2019-04-20 16:33:28 -0600

    Creation of additional classes

---
+++ src/main/java/com/appdetex/sampleparserjavaproject/GoogleAppStoreInfo.kt
@@ -0,0 +1,9 @@
+package com.appdetex.sampleparserjavaproject
+
+data class GoogleAppStoreInfo (
+    val appTitle: String = "Unknown Application",
+    val description: String = "Unknown Application",
+    val publisher: String = "Unknown Application",
+    val price: String = "Free",
+    val rating: Float = 0.0f
+    )
---
+++ src/main/java/com/appdetex/sampleparserjavaproject/GoogleAppStoreScraper.kt
@@ -0,0 +1,52 @@
+package com.appdetex.sampleparserjavaproject
+
+import org.apache.commons.validator.UrlValidator;
+import org.jsoup.Jsoup
+
+
+class GoogleAppStoreScraper(url: String){
+    private val target = url
+
+
+    private fun scrapedApp(): GoogleAppStoreInfo {
+        var appInfo = GoogleAppStoreInfo()
+
+        Jsoup.connect(target).ignoreHttpErrors(true).get().run {
+            select("[class=\"LXrl4c\"]").forEachIndexed { _, element ->
+                val name = element.select("[itemprop=\"name\"]").text()
+                val fullDesc = element.select("[itemprop=\"description\"]").text()
+                val description = fullDesc.split('\n').first()
+
+                val publisher = element.select("div:contains(offered by) > span > div > span").text()
+
+
+                val price = element.select("[itemprop=\"price\"]").attr("content")
+                val rating = element.select("[aria-label*=stars out of five stars]").text().toFloat()
+
+                appInfo = GoogleAppStoreInfo(
+                        appTitle = name,
+                        price = price,
+                        rating = rating,
+                        publisher = publisher,
+                        description = description
+                )
+
+            }
+        }
+
+        return appInfo
+    }
+
+    private fun validUrl(): Boolean {
+        val urlValidator = UrlValidator()
+        return urlValidator.isValid(this.target)
+    }
+
+    fun results(): GoogleAppStoreInfo {
+        if( validUrl() ) {
+            return scrapedApp()
+        }
+
+        return GoogleAppStoreInfo()
+    }
+}
