96a06fedd14c0de717d2cd83d2ff9d16e1a071b0
datictowfliumind <datictowfliumind@gmail.com>
2019-05-02 17:00:07 -0600

    runnable uberjar

---
+++ README.md
@@ -0,0 +1,13 @@
+# SampleParserJavaProject
+
+A utility to scrape html and return parsed json data.
+
+## Usage
+
+Build it:
+
+- ```mvn clean package```
+
+Run it:
+- ```java -jar ./target/sampleparserjavaproject.jar "url-of-site-to-scrape"```
+
--- src/main/java/com/appdetex/sampleparserjavaproject/Main.java
+++ src/main/java/com/appdetex/sampleparserjavaproject/Main.java
@@ -2,15 +2,20 @@ package com.appdetex.sampleparserjavaproject;
 
 /**
  * Main Java Class
- *
+ * <p>
  * This class will use Jsoup to retrieve a provided URL
  * and parse out certain data, printing that data to
  * stdout in a JSON format.
  */
 public class Main {
 
-    public static void main( String[] args ) {
-        // Put code here
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: java -jar sampleparserjavaproject.jar \"url-to-scrape\"");
+            System.exit(0);
+        }
+
+        System.out.println(args[0]);
     }
 
 }
