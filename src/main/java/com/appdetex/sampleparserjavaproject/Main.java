b7f4cc8877f5d72756adb731f493460b920da10a
jweaver <s3v3n.l34f.kl0v3r@gmail.com>
2019-07-15 20:00:21 -0600

    initial commit. looks like i should have been focusing on jsoup

--- src/main/java/com/appdetex/sampleparserjavaproject/Main.java
+++ src/main/java/com/appdetex/sampleparserjavaproject/Main.java
@@ -1,5 +1,24 @@
 package com.appdetex.sampleparserjavaproject;
 
+import com.google.gson.Gson;
+import org.jsoup.Jsoup;
+import org.jsoup.helper.StringUtil;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Main Java Class
  *
@@ -9,8 +28,101 @@ package com.appdetex.sampleparserjavaproject;
  */
 public class Main {
 
-    public static void main( String[] args ) {
-        // Put code here
+
+
+    //TODO: this can be cleaned up a bit. It might be better to set up the RegEx ORs to be 1-1 with data fields we're scraping.
+    public static final String REGEX =
+                    "itemprop=\"([a-zA-Z0-9]*)\" content=\"([\\s\\n\\r\\t\\.\\*!@#$%^&*\\[\\]\\-,'\"_+=;&?=/:.a-zA-Z0-9]*)\"" + //match the easy stuff: Description, price
+                    "|aria-label=\"(Rated) [\\s\\n\\r\\t\\.\\*!@#$%^&*\\[\\]\\-,'_+=;&?=/:.a-zA-Z0-9]*\">([0-9.]*)" + //match rating
+                    "|(developer)\\?id=([a-zA-Z0-9]*)" + //match publisher
+                    "|\"SoftwareApplication\",\"(name)\":\"(([\\s\\w0-9a-zA-Z]*))\",\"image\"" +
+                    ""; //end of regex
+
+    public static void main(String[] args) {
+        Main scraper = new Main();
+        if(args.length == 0) {
+            scraper.printErrorAndUsage(new Exception("missing parameter <URL> during execution"));
+            System.exit(1);
+        }
+
+        String response = scraper.get(args[0]);
+        Map<String,String> items = scraper.parseStringToMap(response);
+        items = scraper.standardizeMapKeys(items);
+        System.out.println(new Gson().toJson(items));
+    }
+
+    public Map<String,String> standardizeMapKeys(Map<String, String> items) {
+        items.put("rating", items.remove("Rated"));
+        items.put("title", items.remove("name"));
+        items.put("publisher", items.remove("developer"));
+        items.remove("url");
+        return items;
+    }
+
+    public Map<String,String> parseStringToMap(String searchString) {
+        Map<String,String> map = new HashMap<String,String>();
+        Matcher matches = Pattern.compile(REGEX)
+                .matcher(searchString);
+        while (matches.find()) {
+            List<String> goodGroupigs = new ArrayList<String>();
+            for (int i = 0; i < matches.groupCount(); i++) {
+                if(matches.group(i) != null) {
+                    goodGroupigs.add(matches.group(i));
+                }
+            }
+            //Group 1 is always the full matched string, the last two groups (roughly key/value) are always the last two in this array.
+            map.put(goodGroupigs.get(goodGroupigs.size()-2), goodGroupigs.get((goodGroupigs.size()-1)));
+        }
+        return map;
+    }
+
+    public String get(String urlString) {
+        StringBuilder sb = new StringBuilder();
+        try {
+            URL url = new URL(urlString);
+            URLConnection urlConn = url.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+            String inputLine;
+
+            while ((inputLine = in.readLine()) != null) {
+                sb.append(inputLine);
+            }
+            in.close();
+
+            tryJsoup(urlString);
+        } catch (IOException e) {
+            printErrorAndUsage(e);
+        } finally {
+            return sb.toString();
+        }
+    }
+
+    public void tryJsoup(String url) throws IOException {
+        Document document = Jsoup.connect(url).get();
+        Element body  =document.body();
+        document.getElementsContainingText("Rated");
+        Elements elements = document.getElementsByAttribute("itemprop");
+        Elements eleemnts2  = document.getElementsByAttribute("developer");
+        String rating = document.getElementsByAttributeValueContaining("aria-label", "Rated").text().trim();
+        String developer = document.getElementsByAttributeValueContaining("", "(developer)\\?id=([a-zA-Z0-9]*)").text();
+        Map<String,String> jsoupMatches = new HashMap<String, String>();
+        document.getElementsContainingOwnText("@context");
+        for(int i = 0; i < elements.size(); i++ ) {
+            Element e = elements.get(i);
+            String itemPropName = e.attr("itemprop");
+            String itemPropValue = e.attr("content");
+
+            if(!itemPropName.equals("") && !itemPropValue.equals("")) {
+                System.out.println(e.attr("itemprop"));
+                System.out.println(e.attr("content"));
+
+            }
+        }
+    }
+
+    private void printErrorAndUsage(Exception e) {
+        System.out.println(e);
+        System.out.println("Write some usage stuff here...");
     }
 
 }
---
+++ src/main/test/RegExTest.java
@@ -0,0 +1,50 @@
+import com.appdetex.sampleparserjavaproject.Main;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RegExTest {
+
+    Main main;
+
+    @BeforeEach
+    private void beforeEach() {
+        main = new Main();
+    }
+
+    @Test
+    public void basicTest() {
+        String testString = "<meta itemprop=\"key\" content=\"value\" />";
+        Map<String,String> testMap = main.parseStringToMap(testString);
+        assertEquals(1, testMap.size());
+        assertEquals("value", testMap.get("key"));
+    }
+
+    @Test
+    public void hugePayloadTest() {
+        String testString =
+                //no match
+                "itemprop=\"offers\" itemscope itemtype=\"https://schema.org/Offer\"><meta "+
+                //matches
+                "itemprop=\"url\" content=\"https://play.google.com/store/apps/details?id=com.mojang.minecraftpe&amp;rdid=com.mojang.minecraftpe&amp;feature=md&amp;offerId\"><meta " +
+                "itemprop=\"price\" content=\"$6.99\"></span></span>$6.99 Buy</button></span></div><c-data id=\"i17\" jsdata=\" GixM9d;_;18 v3Bbmc;unsupported;16 Lq9R7c;unsupported;19 QhZwQc;unsupported;20\"></c-data></c-wiz><c-data id=\"i16\" jsdata=\" sCuaec;_;7 OKeYaf;_;10 pH8t9b;_;8\"></c-data></c-wiz></div></div></div></div></div></div></div><c-data id=\"i7\" jsdata=\" OKeYaf;_;10\"></c-data></c-wiz><c-wiz jsrenderer=\"OvGHec\" jsshadow jsdata=\"deferred-i18\" data-p=\"%.@.[&quot;com.mojang.minecraftpe&quot;,7]]\" data-node-index=\"3;0\" jsmodel=\"hc6Ubd\"><c-wiz jsrenderer=\"mmmrlc\" jsshadow jsdata=\"deferred-i19\" data-p=\"%.@.[&quot;com.mojang.minecraftpe&quot;,7]]\" data-node-index=\"1;0\" jsmodel=\"hc6Ubd GVgNYb\"><div class=\"Rx5dXb\" jscontroller=\"HtFpZ\" jsaction=\"rcuQ6b:npT2md;tPOirb:iksoyf;\" data-slideable-portion-heuristic-width=\"12502\"><div class=\"awJjId\" jsname=\"PjUZJf\" jsaction=\"mousedown:q1TRae; mouseup: V43Ssf; mouseleave:V43Ssf\"><div class=\"gIyxRc\" jsname=\"ZlDilf\"></div></div><div jsname=\"CmYpTb\" class=\"JiLaSd u3EI9e\"><div jsname=\"pCbVjb\" class=\"SgoUSc\"><div class=\"MSLVtf Q4vdJd\" jsname=\"WR0adb\"><img src=\"https://i.ytimg.com/vi/gcf9FM4TbN4/hqdefault.jpg\" class=\"T75of DYfLw\" aria-hidden=\"true\"><div class=\"TdqJUe\"><button aria-label=\"Play Minecraft\" class=\"MMZjL lgooh  \" jscontroller=\"HnDLGf\" jsaction=\"click:axChxd\" jsname=\"pWHZ7d\" data-should-show-kav=\"true\" data-trailer-url=\"https://www.youtube.com/embed/gcf9FM4TbN4?ps=play&amp;vq=large&amp;rel=0&amp;autohide=1&amp;showinfo=0\" data-item-id=\"com.mojang.minecraftpe\" data-item-type=\"7\"></button></div></div><button class=\"Q4vdJd\" aria-label=\"Open screenshot 0\" jscontroller=\"DeWHJf\" jsaction=\"click:O1htCb\" jsname=\"WR0adb\" data-screenshot-item-index=\"0\"><img src=\"https://lh3.googleusercontent.com/28b1vxJQe916wOaSVB4CmcnDujk8M2SNaCwqtQ4cUS0wYKYn9kCYeqxX0uyI2X-nQv0=w720-h310\" srcset=\"https://lh3.googleusercontent.com/28b1vxJQe916wOaSVB4CmcnDujk8M2SNaCwqtQ4cUS0wYKYn9kCYeqxX0uyI2X-nQv0=w1440-h620 2x\" class=\"T75of DYfLw\" aria-hidden=\"true\" width=\"551\" height=\"310\" alt=\"Screenshot Image\" " +
+                "itemprop=\"description\" content=\"Explore! (infinite)*[] worlds and -_+=b,'uild everything;:' f?!@#$%^&*()_-+=:;',.?/1234567890rom\"><h2 aria-label=\"Description\"></h2><div jscontroller=\"IsfMIf\" jsaction=\"rcuQ6b:npT2md\" class=\"PHBdkd\" data-content-height=\"144\" jsshadow><div jsname=\"bN97Pc\" class=\"DWPxHb\" ";
+        Map<String,String> testMap = main.parseStringToMap(testString);
+        assertEquals(3, testMap.size());
+        assertEquals("https://play.google.com/store/apps/details?id=com.mojang.minecraftpe&amp;rdid=com.mojang.minecraftpe&amp;feature=md&amp;offerId", testMap.get("url"));
+        assertEquals("$6.99", testMap.get("price"));
+        assertEquals("Explore! (infinite)*[] worlds and -_+=b,'uild everything;:' f?!@#$%^&*()_-+=:;',.?/1234567890rom", testMap.get("description"));
+
+    }
+
+    @Test
+    public void googleIntegrationTest() {
+        main.get("https://play.google.com/store/apps/details?id=com.mojang.minecraftpe&hl=en-US");
+    }
+}
