737303e6ab9b535081e7f924fa4bd6a8b87fde77
Jason Buchanan <jsnbuchanan@gmail.com>
2021-11-19 17:03:46 -0700

    chore(setup): Converted Java Boilerplate to Kotlin Boilerplate
    - Updated app to use kotlin version 1.6.0
    - Added MainTest.kt
    - Added intellij project files to .gitignore
    - Updated Jsoup dependency from version 1.14.2 to 1.14.3
    - Added Junit dependency

---
+++ src/main/kotlin/com/appdetex/sampleparserjavaproject/Main.kt
@@ -0,0 +1,12 @@
+package com.appdetex.sampleparserjavaproject
+
+/**
+ * Main Kotlin Class
+ *
+ * This class will use Jsoup to retrieve a provided URL
+ * and parse out certain data, printing that data to
+ * stdout in a JSON format.
+ */
+fun main(args: Array<String>) {
+    println("Hello, Appdetex")
+}
---
+++ src/test/kotlin/com/appdetex/MainTest.kt
@@ -0,0 +1,8 @@
+package com.appdetex
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class MainTest {
+
+}
