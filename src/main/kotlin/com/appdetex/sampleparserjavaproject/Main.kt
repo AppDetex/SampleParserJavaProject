118f63987345395f8a627c832512bcc797a20bad
Jason Buchanan <jsnbuchanan@gmail.com>
2021-11-19 18:21:23 -0700

    chore(plan): coming up with a rudimentary plan
    - see Main.kt TODOs

--- src/main/kotlin/com/appdetex/sampleparserjavaproject/Main.kt
+++ src/main/kotlin/com/appdetex/sampleparserjavaproject/Main.kt
@@ -9,4 +9,28 @@ package com.appdetex.sampleparserjavaproject
  */
 fun main(args: Array<String>) {
     println("Hello, Appdetex")
+
+// TODO("setup test noop")
+// TODO("write some tests")
+// TODO("create the GooglePlayApp model")
+// TODO("get commandline argument")
+// TODO("create info log file")
+// TODO("check commandline arg url for propriety")
+// TODO("add commandline help")
+// TODO("create debug log file with raw request data")
+// TODO("create GooglePlayAppParser")
+//         TODO("get app title")
+//         TODO("get first paragraph of the description")
+//         TODO("get publisher's name")
+//         TODO("get price")
+//            TODO("regular price")
+//            TODO("free price")
+// TODO("create info log file entry for each successful retrieval")
+// TODO("create error log file for failed retrievals with error messages")
+// TODO("sanitize content for bad actors")
+// TODO("do a fluency pass. Does everything read correctly")
+// TODO("clean up documentation")
+// TODO("clean up PR")
+// TODO("submit the PR and go celebrate with friends")
+
 }
